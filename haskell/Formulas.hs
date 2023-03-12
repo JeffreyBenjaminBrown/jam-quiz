bb0d9e5ce0e9501f366da4a8dd82f2d2b4989cb2
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-03-12 17:29:48 -0500

    refactor
      change some type signatures, isomorphically
      change some variable names
      add explanatory comments

--- haskell/Formulas.hs
+++ haskell/Formulas.hs
@@ -3,41 +3,56 @@
 {-# LANGUAGE TupleSections #-}
 module Formulas where
 
-import           Control.Monad.State
-import qualified Data.Map as M
-import           System.Random
+import System.Random
 
 import Formulas.Data
+import Util
 
 
 quizNames, quizFormulas, quizVariants, quizModes :: Int -> IO ()
-quizNames =    quizKVList "scale formula" "scale name" formulaNamePairs
-quizFormulas = quizKVList "scale name" "scale formula" nameFormulaPairs
-quizVariants = quizKVList "scale name" "set of variants" nameVariantPairs
-quizModes = quizKVRingList "scale" "mode" $ take 4 nameSetFormulaFamilies
+quizNames =    quizKVList ("scale formula", "scale name") formulaNamePairs
+quizFormulas = quizKVList ("scale name", "scale formula") nameFormulaPairs
+quizVariants = quizKVList ("scale name", "set of variants") nameVariantPairs
+quizModes = quizKVRingList ("scale", "mode") $ take 4 nameSetFormulaFamilies
   -- take 4 to omit the modes of the symmetric scales, which are obvious
 
+{- | @quizKVPair (kType, vType) kv followAction@
+asks a question, then runs @followAction@.
+It asks the question by asking, if @kType@ is @k@,
+for the user to name the corresponding @vType@.
+-}
 quizKVPair :: (Show a, Show b)
-           => String -> String -> (a,b) -> IO ()        -> IO ()
-quizKVPair    keyType   valType   kv       followAction =  do
-          putStrLn $ "\n\n" ++ keyType ++ ": " ++ (show $ fst kv)
-          putStrLn $ valType ++ "?"
+           => (String, String) -> (a,b) -> IO () -> IO ()
+quizKVPair (kType, vType) (k,v) followAction = do
+          putStrLn $ "\n\n" ++ kType ++ ": " ++ show k
+          putStrLn $ vType ++ "?"
           x <- getChar
           case x of 'q' -> return ()
-                    _   -> do putStrLn $ show $ snd kv
+                    _   -> do putStrLn $ show v
                               followAction
 
+{- | Quizzes a user repeatedly from a list of question-answer pairs. -}
 quizKVList :: (Show a, Show b)
-           => String -> String -> [(a,b)] -> Int -> IO ()
-quizKVList keyType valType kvList seed = loop rands
+           => (String, String)
+           -> [(a,b)]
+           -> Int
+           -> IO ()
+quizKVList (kType,vType) kvList seed = loop rands
   where rands = randomRs (0, length kvList - 1) (mkStdGen seed) :: [Int]
         loop rands = do
           let kv = kvList !! head rands
-          quizKVPair keyType valType kv $ loop $ tail rands
+          quizKVPair (kType, vType) kv $ loop $ tail rands
 
+{- | This is very similar to @quizKVList@ above,
+except it takes a @[[(a,b)]]@ instead of a @[(a,b)]@.
+Although I'm not (years later) sure what it does,
+@quizModesg@ is probably instructive. -}
 quizKVRingList :: (Show a, Show b)
-               => String -> String -> [[(a,b)]] -> Int -> IO ()
-quizKVRingList    keyType   valType   kvll         seed = loop rands where
+               => (String, String)
+               -> [[(a,b)]]
+               -> Int
+               -> IO ()
+quizKVRingList (kType, vType) kvll seed = loop rands where
   rands = randomRs (0, 1) (mkStdGen seed) :: [Float]
   loop rands@(a:b:c:moreRands) = do
     let (_,kvl) = kvll !!! a
@@ -47,8 +62,8 @@ quizKVRingList    keyType   valType   kvll         seed = loop rands where
         finish = kvl !! m'
     case m of 0 -> loop moreRands -- try again
               _ -> quizKVPair
-                   ("Starting from " ++ keyType)
-                   ("What is the " ++ valType ++ " " ++ show m
-                    ++ " places (a \"" ++ show (m+1) ++ "th\") higher")
+                   ( ("Starting from " ++ kType)
+                   , ("What is the " ++ vType ++ " " ++ show m
+                      ++ " places (a \"" ++ show (m+1) ++ "th\") higher") )
                    (start,finish)
                    (loop moreRands)
