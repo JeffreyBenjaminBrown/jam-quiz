ce5c93f9f8e8d63a19c14e63c04efb57ca0ab676
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-03-12 17:54:34 -0500

    {- | Quizzes the user on complements modulo an edo -- i.e. n + what = edo. -} quizEdoComplements :: Edo -> Int -> IO ()

--- haskell/EdoMath.hs
+++ haskell/EdoMath.hs
@@ -4,6 +4,15 @@ import Quiz
 import Util
 
 
+{- | Quizzes the user on complements modulo an edo
+-- i.e. n + what = edo. -}
+quizEdoComplements :: Edo -> Int -> IO ()
+quizEdoComplements edo randomSeed = let
+  questions = [ (show edo ++ " minus " ++ show n,
+                 edo - n)
+              | n <- [1..edo] ]
+  in quizKVList ("Q: ", "A: ") questions randomSeed
+
 {- | Quizzes the user on sums modulo an edo. -}
 quizEdo :: Edo -> Int -> IO ()
 quizEdo e randomSeed = let
