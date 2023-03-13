99fdbf61e437fa08f0fcb32f99058e6176f9e6a9
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-03-12 21:38:58 -0500

    +@quizEdoPartitions
      +{- | Assuming `divisor` divides `edo` evenly,
      +@quizEdoPartitions edo divisor randomSeed@
      +quizzes the user on where each note sits
      +relative to the two nearest multiples of @divisor@.
      +
      +Usage example:
      +@
      +quizEdoPartitions 46 23 1
      +@
      +-}
      +quizEdoPartitions :: Edo -> Int -> Int -> IO ()

--- haskell/EdoMath.hs
+++ haskell/EdoMath.hs
@@ -4,6 +4,29 @@ import Quiz
 import Util
 
 
+{- | Assuming `divisor` divides `edo` evenly,
+@quizEdoPartitions edo divisor randomSeed@
+quizzes the user on where each note sits
+relative to the two nearest multiples of @divisor@.
+
+Usage example:
+@
+quizEdoPartitions 46 23 1
+@
+-}
+quizEdoPartitions :: Edo -> Int -> Int -> IO ()
+quizEdoPartitions edo divisor randomSeed = let
+  divisorFloor :: Int -> Int
+  divisorFloor n = divisor * (n `div` divisor)
+  divisorCeiling n = divisorFloor n + divisor
+  questions = [ ( n
+                , ( show df ++ " + " ++ show (n - df) ++ " = " ++ show n,
+                    show n ++ " + " ++ show (dc - n) ++ " = " ++ show dc ) )
+              | n <- [1..edo-1]
+              , let df = divisorFloor n
+                    dc = divisorCeiling n ]
+  in quizKVList ("Q: ", "A: ") questions randomSeed
+
 {- | Quizzes the user on complements modulo an edo
 -- i.e. n + what = edo. -}
 quizEdoComplements :: Edo -> Int -> IO ()
