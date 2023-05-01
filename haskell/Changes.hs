3cf305a2593a02df370b0cbedb783daba0fe3634
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-04-30 19:04:25 -0500

    +quiz_changes2 :: Int -> IO ()
      It's like quiz_changes but better.

--- haskell/Changes.hs
+++ haskell/Changes.hs
@@ -1,45 +1,133 @@
 module Changes where
 
-import Data.List as L
+import Data.List (intersperse)
 
 import Quiz
 
 
--- TODO : Can I incorporate total complexity into this?
-
-sizes, transformations, objects, parameters :: [String]
-
-sizes = [ "greatly", "slightly" ]
-
-transformations =
-  [ "quickly"
-  , "slowly"
-  , "reverse" ] -- in this case, ignore parameter and size
-
-parameters = let
-  base = [ "number"
-         , "speed"
-         , "pitch"
-         , "duration"
-         , "amplitude" ]
-  stat = [ "mean", "variance" ]
-  in [ a ++ " " ++ b
-     | a <- stat, b <- base ]
-
-objects = [ "note"
-          , "root"
-          , "pitch set"
-          , "rhythm"
-          , "melody"
-          , "tone row"
-          , "voice" ]
-
-quiz_gestalt :: Int -> IO ()
-quiz_gestalt seed = quizKVList ("","") changes seed where
+quiz_changes2 :: Int -> IO ()
+quiz_changes2 seed = quizKVList ("","") changes seed where
   changes :: [(String, String)]
-  changes = [ (concat $ L.intersperse ", " [a,b,c,d],
+  changes = [ (concat $ intersperse ", " [ a
+                                         , c
+                                           -- , "speed: " ++ show s
+                                           -- , "degree: " ++ show d
+                                         ],
+                "")
+            | (a,c) <- ac
+            , s <- speeds
+            , d <- degrees ]
+    where
+      ac :: [(String, String)]
+      ac = [ (a,c)
+           | (a,b) <- applicable_changes
+           , c <- b ]
+
+applicable_changes :: [ ( String,
+                          [String] ) ]
+applicable_changes =
+  [ ( "voice",
+      [ "more", "fewer", "apply next change to one of",
+        "vary amplitude across" ] ),
+    -- ( "rhythm",
+    --   [ "more", "fewer",
+    --     "longer extrinsic", "longer intrinsic",
+    --     "shorter extrinsic", "shorter intrinsic",
+    --     "permute", "apply next change to one of",
+    --     "vary amplitude across",
+    --     "odd meter"
+    --     ] ),
+    ( "melody",
+      [ "more", "fewer",
+        "slower", "faster",
+        "more duration", "fewer duration",
+        "more rhythm", "less rhythm",
+        "wider pitch", "narrower pitch",
+        "higher pitch", "lower pitch",
+        "quieter", "louder",
+        "more dynamic", "less dynamic",
+        "longer extrinsic", "longer intrinsic",
+        "shorter extrinsic", "shorter intrinsic",
+        "permute", "reverse",
+        "apply next change to one of",
+        "vary amplitude across",
+        "symmetric", "diatonic", "xen"
+      ] ),
+    --( "tone row",
+    --  [ "more", "fewer",
+    --    "wider pitch", "narrower pitch",
+    --    "higher pitch", "lower pitch",
+    --    "longer extrinsic", "longer intrinsic",
+    --    "shorter extrinsic", "shorter intrinsic",
+    --    "permute", "reverse",
+    --    "apply next change to one of",
+    --    "vary amplitude across",
+    --    "symmetric", "diatonic", "xen"
+    --  ] ),
+    --( "pitch set",
+    --  [ "higher", "lower",
+    --    "longer extrinsic", "longer intrinsic",
+    --    "shorter extrinsic", "shorter intrinsic",
+    --    "permute", "reverse",
+    --    "apply next change to one of",
+    --    "vary amplitude across",
+    --    ] ),
+    ( "feel",
+      [ "funky", "grandiose", "lighthearted",
+        "agitated", "peaceful", "alien",
+        "complex", "simple", "call and response",
+        "symmetric chords", "diatonic chords", "xen pitch", "bichords"
+        ] ) ]
+
+-- | How quickly to make the change.
+newtype Speed = Speed { unSpeed :: Int }
+  deriving (Show, Eq, Enum, Ord, Num)
+
+-- | From 1 to 10 should be more than enough.
+speeds :: [Speed]
+speeds = [1..10]
+
+-- | How great to make the change.
+newtype Degree = Degree { unDegree :: Int }
+  deriving (Show, Eq, Enum, Ord, Num)
+
+-- ^ From 1 to 10 should be more than enough.
+degrees :: [Degree]
+degrees = [1..10]
+
+quiz_changes :: Int -> IO ()
+quiz_changes seed = quizKVList ("","") changes seed where
+  changes :: [(String, String)]
+  changes = [ (concat $ intersperse ", " [a,b,c,d],
                "")
             | a <- sizes
             , b <- transformations
             , c <- parameters
             , d <- objects ]
+    where
+      sizes, transformations, objects, parameters :: [String]
+
+      sizes = [ "greatly", "slightly" ]
+
+      transformations =
+        [ "quickly"
+        , "slowly"
+        , "reverse" ] -- in this case, ignore parameter and size
+
+      parameters = let
+        base = [ "number"
+               , "speed"
+               , "pitch"
+               , "duration"
+               , "amplitude" ]
+        stat = [ "mean", "variance" ]
+        in [ a ++ " " ++ b
+           | a <- stat, b <- base ]
+
+      objects = [ "note"
+                , "root"
+                , "pitch set"
+                , "rhythm"
+                , "melody"
+                , "tone row"
+                , "voice" ]
--- routine.hs
+++ routine.hs
@@ -1,7 +1,7 @@
 -- Could use System.Random.newStdGen,
 -- rather than bumping the seed below.
 
-randomSeed = 13
+randomSeed = 15
 edo = 46
 edoDivisor = 23
 import Changes -- TODO : Why is this necessary?
@@ -10,4 +10,5 @@ quizEdoComplements  edo            randomSeed
 quizEdoPartitions   edo edoDivisor randomSeed
 quizEdoSums         edo            randomSeed
 quizEdoTriads       edo            randomSeed
-quiz_gestalt                       randomSeed
+quiz_changes                       randomSeed
+quiz_changes2                      randomSeed -- better
